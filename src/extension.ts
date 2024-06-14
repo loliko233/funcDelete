3323a89641236ea2158ceea24fd10f4586b843ab
TuWenxuan <2570715834@qq.com>
2024-06-14 14:13:24 +0800

    feat: 完成删除功能

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,15 @@
  * @Author: TuWenxuan
  * @Date: 2024-06-12 14:51:48
  * @LastEditors: TuWenxuan
+ * @LastEditTime: 2024-06-14 10:43:54
+ * @FilePath: /helloworld/src/extension.ts
+ * @Description: 
+ * 
+ */
+/*
+ * @Author: TuWenxuan
+ * @Date: 2024-06-12 14:51:48
+ * @LastEditors: TuWenxuan
  * @LastEditTime: 2024-06-13 14:19:18
  * @FilePath: /helloworld/src/extension.ts
  * @Description: 
@@ -10,56 +19,39 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { parse } from '@babel/parser'
-import traverse from '@babel/traverse'
+import { FuncNode, getFuncNode } from './functionNode.js';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld" is now active!');
-
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('helloworld.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello VS Code111');
-	});
 
-	const editor = vscode.window.activeTextEditor;
-	const code = `const read = ()=>{
-		let str;
+		const editor = vscode.window.activeTextEditor;
 
-		while((str = readline()) != null){
-			let len = str.length/2;
-			let res = 0;
-			for(let i = len - 1; i > 0; i--){
-				let children = [];
-				for(let j = i; j < str.length; j++){
-					if(children.includes(str.slice(j - i, j + 1))){ 
-						res = i;
-						break;
-					}
-					else children.push(str.slice(j - i, j + 1))
-				}
-			}
-			search(next, str);
+		if (!editor) {
+			return;
+		};
+
+		const code = editor.document.getText();
+		const index = editor.document.offsetAt(editor.selection.active);
+
+		const funcNode: FuncNode | undefined = getFuncNode(code, index);
+
+		if (!funcNode) {
+			return;
 		}
-	}`;
 
-	const art = parse(code);
-	console.log(art);
-
-	if (editor) {
 		editor.edit((editBuilder) => {
-			editBuilder.insert(new vscode.Position(0, 0), 'Hello World!');
-			editBuilder.delete(new vscode.Range(0, 0, 1, 0));
+			editBuilder.delete(new vscode.Range(funcNode.start.line - 1, funcNode.start.column, funcNode.end.line - 1, funcNode.end.column));
 		});
-	}
+	});
 
 	context.subscriptions.push(disposable);
 }
